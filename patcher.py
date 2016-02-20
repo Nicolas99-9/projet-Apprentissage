ae6adbfd162dd8e86db0ba2622dda5bd1d9a0857
Nicolas <bougie.nicolas@gmail.com>
2016-02-20 15:13:23 +0100

    new function to cut the image

--- patcher.py
+++ patcher.py
@@ -18,62 +18,65 @@ class Patcher:
         print("Number of patches in the image : ",  self.number_patches*self.number_patches) 
 
     def get_patches_from_image(self,image):
-        #a[:,1] get the second colum
-        after = np.array(image).reshape((self.imageSize,self.imageSize*3),order='F')
-        print(after.shape)
+        first = image[:1024]
+        second = image[1024:2048]
+        last = image[1024:]
+        image = []
+        for i in range(1024):
+            image.append(first[i])
+            image.append(second[i])
+            image.append(last[i])
+        after = np.array(image).reshape((self.imageSize,self.imageSize*3))
         patches= []
         for i in range(self.number_patches):
             for j in xrange(0,self.number_patches):
-                tmp = after[i*self.sizeSquare:i*self.sizeSquare+self.sizeSquare,j*self.sizeSquare:j*self.sizeSquare+self.sizeSquare*3]
+                print(i*self.sizeSquare,i*self.sizeSquare+self.sizeSquare,j*self.sizeSquare*3,j*self.sizeSquare*3+self.sizeSquare*3)
+                tmp = after[i*self.sizeSquare:i*self.sizeSquare+self.sizeSquare,j*self.sizeSquare*3:j*self.sizeSquare*3+self.sizeSquare*3]
                 patches.append(tmp.reshape(self.sizeSquare*self.sizeSquare*3))
-        #print("after : ",after)
-        #print("--------------------------------------")
-        #print(patches)
         return list(patches)
 
     def show_patches(self,patches):
         #print("l1",len(patches),len(patches[0]))
         tmp = np.array(patches)
         print("debugf ",tmp.shape)
-        tmp = tmp/255.0 
         to_display = []
-        for i in range(len(patches)):
-            to_add = []
-            for j in xrange(0,(self.sizeSquare)*3,3):
-                to_add.append(patches[i][j:j+3])
-            to_display.append(to_add)
+        to_add = []
+        count =0
+        for j in xrange(0,len(patches),3):
+            to_add.append([tmp[j]/255.0  ,tmp[j+1]/255.0  ,tmp[j+2]/255.0 ])
+            count +=1
+            if(count%(self.sizeSquare)==0):
+                print(j)
+                to_display.append(to_add)
+                to_add = [] 
+                count =0
         to_display = np.array(to_display)
         plt.imshow(to_display)
         plt.show()
-        
-#test
+    
+    def debug(self,images,patches):
+        for i in range(20):
+            print("images : ",images[i],images[i+1024],images[i+2048] , " patch : ", patches[0][i],patches[0][i+1],patches[0][i+2])
 
-def unpickle(file):
-    fo = open(file, 'rb')
-    dict = cPickle.load(fo)
-    fo.close()
-    return dict
+    def show_image(img):
+        to_display = []
+        count = 0
+        tmp = []
+        for i in range(1024):
+            tmp.append([img[i]/255.0,img[i+1024]/255.0,img[i+2048]/255.0])
+            count +=1
+            if(count%32==0):
+                count = 0
+                to_display.append(tmp)
+                tmp = []
+        to_display = np.array(to_display)
+        plt.imshow(to_display)
+        plt.show()
 
-def show_image(img):
-    to_display = []
-    count = 0
-    tmp = []
-    for i in range(1024):
-        tmp.append([img[i]/255.0,img[i+1024]/255.0,img[i+2048]/255.0])
-        count +=1
-        if(count%32==0):
-            count = 0
-            to_display.append(tmp)
-            tmp = []
-    to_display = np.array(to_display)
-    plt.imshow(to_display)
-    plt.show()
 
-dicos = unpickle("cifar-10-batches-py/data_batch_1")
+'''
 patcher = Patcher(16,32)
-patcher.show_informations()
-patches = patcher.get_patches_from_image(dicos['data'][0])
-print("shape des patches,",np.array(patches).shape)
-print(patches[0])
-show_image(dicos['data'][0])
+patches = patcher.get_patches_from_image(dicos['data'][25])
+show_image(dicos['data'][25])
 patcher.show_patches(patches[0])
+'''
